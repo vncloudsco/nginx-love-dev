3a446daf09d164a7b35fcca55b56120c113ed642
vncloudsco <contact@manhtuong.net>
2025-10-10 05:04:01 +0000

    Implement code changes to enhance functionality and improve performance

--- docker/start.sh
+++ docker/start.sh
@@ -7,15 +7,5 @@ rm -f /var/run/nginx.pid
 echo "🚀 Starting Nginx..."
 nginx -g "daemon off;" &
 
-# Wait for Postgres to be ready
-echo "⏳ Waiting for Postgres to be ready..."
-sleep 30
-
-# Wait prisma to be ready
-pnpm prisma:generate
-pnpm exec prisma migrate deploy
-pnpm prisma:seed
-touch .seeded
-
 echo "🟢 Starting Node.js API..."
 node /app/apps/api/dist/index.js
